a8bb2a7bdb0e09a35faf340a49cc299732612a4f
Ryan Zielke <NeoAlchemy@gmail.com>
2021-04-11 22:15:25 -0500

    blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Block = SpriteKind.create()
+}
 function initializePlayer () {
     falcon = sprites.create(img`
         . . . . . 7 . . 7 . . . . . 
@@ -21,9 +24,9 @@ function initializePlayer () {
         . . 7 7 7 7 7 7 7 7 7 7 . . 
         . . . 7 7 7 7 7 7 7 7 . . . 
         `, SpriteKind.Player)
-    for (let value of tiles.getTilesByType(assets.tile`millenium falcon`)) {
-        tiles.placeOnTile(falcon, value)
-        tiles.setTileAt(value, assets.tile`transparency16`)
+    for (let height of tiles.getTilesByType(assets.tile`millenium falcon`)) {
+        tiles.placeOnTile(falcon, height)
+        tiles.setTileAt(height, assets.tile`transparency16`)
     }
     controller.moveSprite(falcon, 100, 0)
 }
@@ -53,24 +56,45 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 function setupGame () {
     scene.centerCameraAt(0, 0)
 }
+function blocker (x: number, y: number) {
+    for (let height = 0; height <= 20; height++) {
+        for (let width = 0; width <= 20; width++) {
+            mySprite = sprites.create(img`
+                7 7 
+                7 7 
+                `, SpriteKind.Block)
+            mySprite.setPosition(width + x, height + y)
+        }
+    }
+}
 function buildLevel () {
     scene.setBackgroundColor(15)
     tiles.setTilemap(tilemap`level1`)
 }
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Block, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    sprite.destroy()
+})
 sprites.onCreated(SpriteKind.Projectile, function (sprite) {
     projectileExists = 1
 })
+function initializeBlocker () {
+    blocker(20, 60)
+    blocker(70, 60)
+    blocker(120, 60)
+}
 // https://www.deviantart.com/mattcantdraw/art/Star-Wars-Space-Invaders-286179610
 let projectileExists = 0
+let mySprite: Sprite = null
 let projectile: Sprite = null
 let firedProjectile = 0
 let falcon: Sprite = null
 setupGame()
 buildLevel()
 initializePlayer()
+initializeBlocker()
 game.onUpdate(function () {
     if (projectileExists == 1) {
-        console.logValue("x", projectile.y)
         if (projectile.y == 3) {
             firedProjectile = 0
         }
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -5,14 +5,14 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`0b00080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000203000203000203000203000000000000000000000000000000010000000000000000000000000000000000`, img`
+            case "level1":return tiles.createTilemap(hex`0b00080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000`, img`
 . . . . . . . . . . . 
 . . . . . . . . . . . 
 . . . . . . . . . . . 
@@ -21,7 +21,7 @@ namespace myTiles {
 . . . . . . . . . . . 
 . . . . . . . . . . . 
 . . . . . . . . . . . 
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,10 +31,10 @@ namespace myTiles {
             case "transparency16":return transparency16;
             case "millenium falcon":
             case "tile1":return tile1;
-            case "myTile":
-            case "tile2":return tile2;
             case "myTile0":
             case "tile3":return tile3;
+            case "myTile":
+            case "tile2":return tile2;
         }
         return null;
     })
